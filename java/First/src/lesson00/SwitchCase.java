4d144384bba4ff3b13e06ece66c3d3e385d9933a
sakura09040904 <kyj23520@naver.com>
2021-07-20 10:01:40 +0900

    2021-07-19

---
+++ java/First/src/lesson00/SwitchCase.java
@@ -0,0 +1,29 @@
+package lesson00;
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please input your score:");
+		int score = (input.nextInt())/10;
+		
+		switch(score) {
+		case 9:
+			System.out.println("Your grade is A");
+			break;
+		case 8:
+			System.out.println("Your grade is B");
+			break;
+		case 7:
+			System.out.println("Your grade is C");
+			break;
+		case 6:
+			System.out.println("Your grade is D");
+			break;
+			
+		}
+
+	}
+
+}
